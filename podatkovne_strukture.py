f446449d7f08132c0a3e0f49187b7e8668177f1b
timen.stepisnik.perdih@in516ht.com <timen.stepisnik.perdih@in516ht.com>
2022-11-08 14:17:41 +0100

    Naloge

---
+++ podatkovne_strukture.py
@@ -0,0 +1,19 @@
+## TUPLES:
+def vrni_dva_podatka(seznam: list):
+    return(sum(seznam), len(seznam))
+
+
+seznam_stevil = [1,2,3,4]
+# popped = seznam_stevil.pop()
+popped = seznam_stevil.pop(0)
+print(popped)
+print(seznam_stevil)
+## STACK 
+# list.pop()
+# list.append()
+# String oklepajev, preveri ce so ok ali ne -> "()[()](()){}"
+
+
+
+## ARRAY:
+# list of lists: [[]]
